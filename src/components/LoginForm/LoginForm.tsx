ed5d52a267cff857dd4b2ba596a8a7e9011ef3c1
musanif <musanifali300@gmail.com>
2023-10-28 13:50:50 +0500

    create routes

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,52 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Form, Input, Button, message } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { login } from "./api-client";
 import { saveToken, decodeToken } from "./tokenUtils";
+import { Navigate, useNavigate } from "react-router-dom";
 
 interface LoginFormValues {
   email: string;
   password: string;
 }
+
 interface Props {
   onSwitchForm: () => void;
 }
+
 const LoginForm = ({ onSwitchForm }: Props) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
     if (token) {
       const decodedToken = decodeToken(token);
       if (decodedToken.isAdmin) {
-        console.log("Admin");
-        // Redirect to the admin page
+        navigate("/admin");
       } else {
-        console.log("home");
-        // Redirect to the home page
+        navigate("/user1");
       }
     }
   }, []);
 
   const onFinish = async (values: LoginFormValues) => {
     try {
+      setLoading(true); // Start the loading animation
+
       const response = await login(values);
       const token = response.data;
       console.log(token); // Handle the token as needed
-      message.success("Login successful!");
-
-      saveToken(token);
 
       const decodedToken = decodeToken(token);
 
-      if (decodedToken.isAdmin) {
-        console.log("Admin");
-        // Redirect to the admin page
-      } else {
-        console.log("User");
-        // Redirect to the home page
-      }
+      // Stop the loading animation after a delay of 2 seconds
+      setTimeout(() => {
+        setLoading(false);
+
+        message.success("Login successful!");
+
+        saveToken(token);
+
+        if (decodedToken.isAdmin) {
+          navigate("/admin");
+        } else {
+          navigate("/user1");
+        }
+      }, 2000);
     } catch (error: any) {
+      setLoading(false); // Stop the loading animation
+
       if (error?.response?.status === 403) {
         message.warning("Invalid credentials");
       } else {
@@ -56,7 +68,7 @@ const LoginForm = ({ onSwitchForm }: Props) => {
   };
 
   return (
-    <div className={`flex justify-center items-center h-screen `}>
+    <div className={`flex justify-center items-center h-screen`}>
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
@@ -66,8 +78,8 @@ const LoginForm = ({ onSwitchForm }: Props) => {
           boxShadow: "0px 8px 15px rgba(0, 0, 0, 0.1)",
         }}
       >
-        <div className={`w-full max-w-md bg-white rounded-lg shadow-lg p-8 `}>
-          <h2 className={`text-2xl font-bold mb-4 `}>Log In</h2>
+        <div className={`w-full max-w-md bg-white rounded-lg shadow-lg p-8`}>
+          <h2 className={`text-2xl font-bold mb-4`}>Log In</h2>
           <Form
             name="loginForm"
             className="space-y-4"
@@ -104,10 +116,11 @@ const LoginForm = ({ onSwitchForm }: Props) => {
               <Button
                 type="primary"
                 htmlType="submit"
-                className={`w-full bg-blue-500 hover:bg-blue-700 `}
+                className={`w-full bg-blue-500 hover:bg-blue-700`}
                 style={{
                   borderRadius: "4px",
                 }}
+                loading={loading} // Add loading prop to show loading animation
               >
                 Log In
               </Button>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  ShopOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Header>header</Header>
+      <Layout>
+        <Sider>left sidebar</Sider>
+        <Content>main content</Content>
+        <Sider>right sidebar</Sider>
+      </Layout>
+      <Footer>footer</Footer>
+    </Layout>
+  );
+};
+
+export default App;
